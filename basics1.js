3cfd652d54c951b08b5ec140d490177159eba11d
blessankurien <129526078+blessankurien@users.noreply.github.com>
2023-03-31 20:18:47 +0530

    Initial commit

---
+++ basics1.js
@@ -0,0 +1,58 @@
+//nod  a=5;
+// var a=8;
+//console.log(a);
+//et b=10;
+//console.log(b);
+{
+    let b=8;
+    // console.log(b);
+}
+// console.log(b);
+//const pi=3.14;
+//console.log(pi);
+
+//var a=['meera', 'nithya',10];
+//console.log(a[2]);
+//a.push('orange')
+//console.log(a)
+//a.pop();
+//console.log(a)
+
+//let car={
+    //model:'i10',
+    //color:'blue',
+   // brand:'Hyundai'
+//}
+//console.log(car.color);
+
+//let human={
+    //ear:'white',
+   // nose:'black',
+   // neck:'red'
+//}
+//console.log(human.nose);
+
+var person=new Object();
+person.name="Maya";
+person.age=20;
+console.log(person);
+
+// Operators
+var a=2;
+a++;
+console.log(a);
+
+//Functions
+function addt(a,b) //Function Definition
+{
+        var sum=a+b;
+        console.log(sum);
+}
+addt(6,7);//function call
+
+function addt(a,b) //Function Definition
+{
+        var sum=a+b;
+        return sum;
+}
+addt(6,7);//function call
---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("hello world");
+
+//let i=10;
+
+//console.log(i);
+
+//for(let i=0;i<=10;i++)
+//{
+//console.log(i);
+//}
+
+//let sum=0;
+//for(let i=1; i<=10; i++)
+//{
+  //  sum=sum+i;
+//}
+//console.log('sum of first 10 natural number is', sum);
+
+//while loop
+
+let i=0;
+while(i<=10)
+{console.log(i);
+i++;
+}
